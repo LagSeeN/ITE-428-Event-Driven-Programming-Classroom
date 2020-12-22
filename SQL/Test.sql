e0cec144e39aaa76145816965a85f3c0356af027
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2020-12-22 11:06:07 +0700

    เพิ่มแบบฝึกหัดข้อ 6 และไฟล์ test sql

--- SQL/Test.sql
+++ SQL/Test.sql
@@ -1,2 +1,9 @@
 SELECT * FROM Products
 WHERE UnitPrice > (SELECT avg(UnitPrice) FROM Products);
+
+SELECT *,UnitPrice * UnitsInStock AS 'Value in Stock' FROM Products
+WHERE  UnitPrice * UnitsInStock > 3000;
+
+SELECT CategoryName, count(DISTINCT ProductId) FROM Categories
+LEFT OUTER JOIN Products
+ON Categories.CategoryID = Products.CategoryId;
